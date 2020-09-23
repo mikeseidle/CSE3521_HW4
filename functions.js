7c7f63d57109740191f6eb57a13646815b172bb2
mikeseidle <seidle.8@osu.edu>
2020-09-23 14:42:32 -0400

    is_terminal guess

--- functions.js
+++ functions.js
@@ -7,5 +7,55 @@ function is_terminal(board) {
     * Return false if the game is incomplete
     *************************/
     
+    // Checking base case if the board is full and game is a tie
+    let isFull = true;
+    for(i = 0 ; i < 9; i++){
+      if(board[i] == -1){
+        isFull = false;
+      }
+    }
+    if(isFull){
+      return true;
+    }
+
+    //Check the other 9 states that are wins
+
+    for(j =0; j <= 6; j++){
+      
+      if(j == 0 || j == 3 || j == 6){
+        let a = j;
+        let b = j +1;
+        let c = j+2;
+        if(board[a] == board[b] && board[b] == board[c] && board[a]== board[c]){
+          return true;
+        }
+      }
+      if(j == 0 || j == 1 || j == 2){
+        let a = j;
+        let b = j +3;
+        let c = j+6;
+        if(board[a] == board[b] && board[b] == board[c] && board[a]== board[c]){
+          return true;
+        }
+      }
+      if(j == 0){
+        let a = j;
+        let b = j +4;
+        let c = j+8;
+        if(board[a] == board[b] && board[b] == board[c] && board[a]== board[c]){
+          return true;
+        }
+      }
+      if(j == 2){
+        let a = j;
+        let b = j +2;
+        let c = j+4;
+        if(board[a] == board[b] && board[b] == board[c] && board[a]== board[c]){
+          return true;
+        }
+      }
+
+    }
+
     return false;
   }
--- tictactoe.js
+++ tictactoe.js
@@ -64,8 +64,8 @@ function tictactoe_minimax(board,cpu_player,cur_player) {
   //Return results gathered from all sucessors (moves).
   //Which was the "best" move?  
   return {
-    move: /* What do you return here? */,
-    score: /* And here? */
+    move: []/* What do you return here? */,
+    score: []/* And here? */
   };
 }
 
@@ -77,6 +77,57 @@ function is_terminal(board) {
   * Return true if the game is finished (i.e, a draw or someone has won)
   * Return false if the game is incomplete
   *************************/
+  
+  // Checking base case if the board is full and game is a tie
+  let isFull = true;
+  for(i = 0 ; i < 9; i++){
+    if(board[i] == -1){
+      isFull = false;
+    }
+  }
+  if(isFull){
+    return true;
+  }
+
+  //Check the other 9 states that are wins
+
+  for(j =0; j <= 6; j++){
+    
+    if(j == 0 || j == 3 || j == 6){
+      let a = j;
+      let b = j +1;
+      let c = j+2;
+      if(board[a] == board[b] && board[b] == board[c] && board[a]== board[c]){
+        return true;
+      }
+    }
+    if(j == 0 || j == 1 || j == 2){
+      let a = j;
+      let b = j +3;
+      let c = j+6;
+      if(board[a] == board[b] && board[b] == board[c] && board[a]== board[c]){
+        return true;
+      }
+    }
+    if(j == 0){
+      let a = j;
+      let b = j +4;
+      let c = j+8;
+      if(board[a] == board[b] && board[b] == board[c] && board[a]== board[c]){
+        return true;
+      }
+    }
+    if(j == 2){
+      let a = j;
+      let b = j +2;
+      let c = j+4;
+      if(board[a] == board[b] && board[b] == board[c] && board[a]== board[c]){
+        return true;
+      }
+    }
+
+  }
+
   return false;
 }
 
