d97effd50f62d5d3f47049a8945330aabc56d379
Michael Seidle <mvseidle0720@gmail.com>
2020-09-23 14:19:07 -0400

    added funtion.js

---
+++ functions.js
@@ -0,0 +1,11 @@
+function is_terminal(board) {
+    ++helper_eval_state_count; //DO NOT REMOVE
+    
+    /*************************
+    * TASK: Implement the terminal test
+    * Return true if the game is finished (i.e, a draw or someone has won)
+    * Return false if the game is incomplete
+    *************************/
+    
+    return false;
+  }
